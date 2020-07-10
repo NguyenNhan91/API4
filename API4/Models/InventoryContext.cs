fd2c5cde5d110f268e88e45a0e1848604016a277
Nguyễn Thái Nhân <thainhan@hotmail.com.vn>
2020-07-10 11:37:32 +0700

    Add project files.

---
+++ API4/Models/InventoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API4.Models
+{
+    public partial class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+    }
+}
